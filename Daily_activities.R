6f7ee9a53cdf1c4cbaf72d0801bd05cc0e994b3a
vladperesad <holleywallbridge@gmail.com>
2023-03-16 16:45:40 +0800

    Loaded up datasets and R files

---
+++ Daily_activities.R
@@ -0,0 +1,155 @@
+#Load up the libraries----
+library(tidyverse)
+library(skimr)
+library(ggcorrplot)
+library(lubridate)
+
+#Set up a working directory
+setwd("C:/Users/holle/Documents/DataAnalysis/Project_1/Fitabase Data 4.12.16-5.12.16")
+
+
+
+#DataFrames----
+daily_activity_merged <- read.csv("dailyActivity_merged.csv")
+
+#Exploring the structure of the dataset and shaping it appropriately----
+
+str(daily_activity_merged)
+
+
+daily_activity_merged$Id <- as.factor(daily_activity_merged$Id)
+#turn Id to a factor
+
+daily_activity_merged <- daily_activity_merged %>% 
+  mutate(Date = strptime(ActivityDate, "%m/%d/%Y")) %>% 
+  select(!(ActivityDate))
+#turn date into a proper format YYYY/MM/DD
+
+daily_activity_merged$Date <- as.Date(daily_activity_merged$Date)
+#and turn it into a date type
+
+daily_activity_merged$DayOfWeek = ifelse(wday(daily_activity_merged$Date)== 1,7, wday(daily_activity_merged$Date)-1)
+#pull number of the day of the week from the date 1 for Mon 7 for Sun
+
+#Exploring and cleaning the dataset----
+
+length(unique(daily_activity_merged$Id))
+#count distinct Ids (should be 30, but 33 instead)
+
+daily_activity_merged <- daily_activity_merged %>%
+  group_by(Id) %>% 
+  mutate(Count = length(Id)) %>%
+  filter(Count > 7) %>% 
+  select(Id, Count, Date, everything()) %>% 
+  ungroup()
+#add another column that has a count of days each Id submitted data
+#get rid of records that count less than 7 days (1 full week)
+
+summary(daily_activity_merged)
+#all the values seem to make sense (no negative values, or any numbers out of normal range)
+
+skimr::skim(daily_activity_merged)
+# taking a look at the dataset using skimr, paying attention to n_missing and complete_rate
+
+anyDuplicated(daily_activity_merged)
+#checking for duplicates
+
+#create a barchart that represents time people spend in each kind of activity----
+
+DAM_mins_long <- daily_activity_merged %>% 
+  select(Date, DayOfWeek, VeryActiveMinutes, FairlyActiveMinutes, LightlyActiveMinutes, SedentaryMinutes ) %>% 
+  gather(key = IntensityLevel, value = Minutes, c(3:6))
+#select data that represents time spent doing each activity and turn it to a long format
+
+#and plot it
+(barplot_act <-ggplot(DAM_mins_long, aes(x = reorder(IntensityLevel, desc(Minutes)), y = Minutes, fill = IntensityLevel))+
+    geom_bar(stat = "identity")+
+    theme_minimal()+
+    scale_x_discrete(labels = c("SedentaryMinutes"="Sedentary",
+                                "LightlyActiveMinutes" = "Lightly active",
+                                "VeryActiveMinutes" = "Very Active",
+                                  "FairlyActiveMinutes" = "Fairly Active"))+
+    theme(legend.position = "none")+
+    labs(title = "Most and least popular activities",
+         subtitle = "How much time users spend performing each activity",
+         x = ""))
+#very obvious that people spend more time sitting than all other activities together
+
+
+#create a correlation matrix to see if any values correlate with others----
+
+str(sub_corr_mat_2)
+
+corr_mat <- daily_activity_merged %>% 
+  select(!c(Id, Date, TotalDistance, TrackerDistance, LoggedActivitiesDistance))
+#get rid of columns that don't make any difference as they are calculated based on others
+corr_mat <- round(cor(corr_mat),1)
+#create a correlation matrix and round values for easier reading
+(corr_plot <- ggcorrplot(cor(corr_mat), hc.order = TRUE, type = "lower", lab = TRUE)+
+    theme_minimal()+
+    labs(x = "",
+         y = "")+
+    theme(legend.position = "none",
+          axis.text.x = element_text(angle = 45, hjust = 1)))
+#plotting the corr matrix
+#such strong correlation between LAD and LAM is explained by the fact that 
+#values in the first are calculated based on the second 
+#same for MAD and FAM as well as VAD and VAM
+
+
+#getting deeper investigating correlations----
+
+#plot scatterplot showing correlation between number of steps and calories burnt
+(steps_cal <- ggplot(daily_activity_merged, aes(x = TotalSteps, y = Calories))+
+    geom_point()+
+    stat_smooth(method = "lm", color = "#fe7f2d")+
+    theme_minimal()+
+    labs(title = "Steps vs. calories",
+        subtitle = "Correlation between number of steps taken and amount of calories burned",
+        x = "Steps"))
+#more steps made = more calories burnt !genius!
+#the point is it seems like a lot of people consider taking steps 
+#as calories burning activity
+
+(steps_dist <-ggplot(daily_activity_merged, aes(x = TotalSteps, y =TrackerDistance))+
+    geom_point()+
+    stat_smooth(method = "lm", color = "#fe7f2d")+
+    theme_minimal()+
+    labs(title = "Distance (km) vs. steps",
+         subtitle = "Correlation between number of steps counted by a pedometer and distance covered according to device's GPS tracker ",
+         x = "Steps",
+         y = "Distance (km)"))
+#tracker distance seems to directly correlate with number of steps taken 
+#which means that distance measured by GPS correlates with number of steps counted by a pedometer in a device
+#which means not thread mill
+#hence device used outside, needs to be waterproof for rain and legible under bright sun 
+
+
+#count how many times each day is represented in the dataset
+day_of_week_cnt <- daily_activity_merged %>% 
+  group_by(DayOfWeek) %>% 
+  summarize(count = n())
+#turnes out Tuesday is represented more than any other day, so simple sum won't suffice
+#theres a need to calculate the mean for each day of the week
+
+
+#find out which day of the week people burn more calories = being more active
+
+cal_mean <- mean(daily_activity_merged$Calories)
+cal_by_day <- daily_activity_merged %>% 
+  group_by(DayOfWeek) %>% 
+  summarize(CaloriesMean = mean(Calories),
+            RelativeVal = 100*(mean(Calories)-cal_mean)/cal_mean)
+
+(cal_by_day_plot <- ggplot(cal_by_day, aes(x = as.factor(DayOfWeek), y = RelativeVal, fill = RelativeVal))+
+    geom_bar(stat = "identity")+
+    scale_x_discrete(labels = c("Monday", "Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"))+
+    theme_minimal()+
+    labs(title = "Amount of calories burnt by day of week",
+         x = "",
+         y = "Relative calories (%)")+
+  theme(legend.position = "none"))
+#there is a clear bend over the middle of the week where people feel the least motivated
+#need to prompt em
+#they feel the most motivated at the beginning and on the weekend
+#makes sense to offer new plans and subscriptions those days - more likely to get a sign up
---
+++ Hourly_sleep.R
@@ -0,0 +1,83 @@
+#Load up the libraries----
+library(tidyverse)
+library(lubridate)
+library(skimr)
+
+#Set up a working directory
+setwd("C:/Users/holle/Documents/DataAnalysis/Project_1/Fitabase Data 4.12.16-5.12.16")
+
+#import dataset----
+minute_sleep_merged <- read.csv("minuteSleep_merged.csv")
+
+
+#exploring the dataset-----
+
+skim(minute_sleep_merged)
+#use skim function to see if data makes sense and take a look at its structure
+
+minute_sleep_merged <- distinct(minute_sleep_merged)
+#remove the duplicates if theyre present (543 rows)
+
+minute_sleep_merged <- minute_sleep_merged %>% 
+  mutate(DateNew = strptime(date, "%m/%d/%Y %I:%M:%S %p")) %>% 
+  tidyr::separate(DateNew, c("Date", "Time"), sep = " ", remove = FALSE) %>%  
+  filter(Time >= "20:00:00" | Time <= "08:00:00") %>% 
+  select(Id, LogId = logId, DateTime = DateNew, Value = value)
+#shape date column into appropriate format YYYY-MM-DD HH:MM:SS
+#separate date and time columns
+#filter records with times between 8:00 and 20:00 as we're not interested in naps
+#reshape the dataframe a little bit
+
+minute_sleep_merged$DateTime <- as.POSIXct(minute_sleep_merged$DateTime)
+#turn date column into proper datetime type
+
+skim(minute_sleep_merged)
+#run the skim function again to see how the data changed (188521 vs 172148)
+#value mean (from 1.10 to 1.09)
+#value std (from 0,329 to 0.320)
+#data makes sense in terms of values and dates
+
+summary(minute_sleep_merged)
+#dataset contains records of sleep dates and times( from 2016-04-11 to 2016-05-12) 
+#and quality of sleep for every minute graded from 1 (sleeping), 2(restless), 3(awake)
+
+
+MSM_subset <- minute_sleep_merged %>%
+  mutate(ShiftedTime = DateTime+hours(4)) %>% 
+  tidyr::separate(ShiftedTime, c("Date", "SftTime"), sep = " " ) %>% 
+  select(Id, LogId, SftTime)
+#create a 4 hour offset (so that is is plotted from 20 to 23 and then from 0 to 12)
+#separate an offset date_time into date and shifted time
+#and select columns we need for further calculations
+
+MSM_subset <- MSM_subset  %>% 
+  group_by(LogId) %>% 
+  slice(1)
+#create a subset that contains times at which each sleep began
+
+MSM_new <- right_join(minute_sleep_merged, MSM_subset, by = c("LogId", "Id"))
+#performing a right join to join dataset containing start sleep times to a main one 
+
+MSM_new <- MSM_new %>%
+  select(Id, LogId, DateTime, SftTime, Value) %>% 
+  group_by(Id, LogId, SftTime) %>% 
+  summarise(MeanValue = mean(Value)) %>% 
+  mutate(Date = today()) %>%
+  unite("DateTime", 5,3, sep = " ")
+#calculate mean sleep quality value for each sleep
+
+MSM_new$DateTime <- as_datetime(MSM_new$DateTime)
+
+(sleep_quality_vs_time <- ggplot(MSM_new, aes(x =DateTime, y = MeanValue))+
+    geom_point()+
+    geom_smooth(color = "#fe7f2d")+
+    scale_x_datetime(date_breaks = "2 hours",
+                     date_labels = c("21:00","21:00","23:00","01:00","03:00","05:00","07:00"))+
+    scale_y_continuous(limits = c(1, 1.6))+
+    theme_minimal()+
+    labs(title = "Bedtime vs. quality of sleep",
+         subtitle = "How the time we go to bed affects our sleep",
+         x = "",
+         y = "Sleep quality index"))
+
+ggsave(sleep_quality_vs_time, filename = "bedtime_vs_sleep.png", height = 9, width = 16)  
